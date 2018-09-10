a6223d29ebe04e905793d2e8f75d3d4d57d3a14b
benz <5706021630097@fitm.kmutnb.ac.th>
2018-09-10 22:18:46 +0700

    i edit file insertOffice.js of node

--- nodeFinFrog/accessDatabase/insertOfficer.js
+++ nodeFinFrog/accessDatabase/insertOfficer.js
@@ -8,11 +8,12 @@ var connection =connection.connection();
 module.exports={
     insertOfficer: function(req,res,code,name,subname,file,address,user,pass) {
  
-        connection.query("INSERT INTO customer values(?,?,?,?,?,?,?,?,?,?,?)",[user,pass,name,subname,null,null,address,'officer',file,code,"ทำงาน"] ,function (err, result, fields) {
-            console.log(result);
+        connection.query("INSERT INTO customer values(?,?,?,?,?,?,?,?,?,?,?,?)",[user,pass,name,subname,null,null,address,'officer',file,code,"ทำงาน",null] ,function (err, result, fields) {
+            console.log(code+' '+name+' '+subname+' '+file+' '+address+' '+user+' '+pass);
+            //console.log(err);
             if (err) { res.setHeader('Access-Control-Allow-Origin', '*'); return (res.json({'qreury':false}))}
             
-                    //console.log(result);
+                   
                     res.setHeader('Access-Control-Allow-Origin', '*');
                     res.json({'qreury':true});
                    });
